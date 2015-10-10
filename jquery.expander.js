9a322f426f9c802a23d462db45c67196d9da6293
Karl Swedberg <kswedberg@gmail.com>
2015-10-10 14:07:27 -0400

    Release 1.6.0

--- jquery.expander.js
+++ jquery.expander.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Expander Plugin - v1.5.1 - 2014-10-05
+ * jQuery Expander Plugin - v1.6.0 - 2014-10-05
  * http://plugins.learningjquery.com/expander/
  * Copyright (c) 2014 Karl Swedberg
  * Licensed MIT (http://www.opensource.org/licenses/mit-license.php)
@@ -15,7 +15,7 @@
   }
 })(function($) {
   $.expander = {
-    version: '1.5.1',
+    version: '1.6.0',
     defaults: {
       // the number of characters at which the contents will be sliced into two parts.
       slicePoint: 100,
